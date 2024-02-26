278f58781090485ec12b4a7b95946b9bd0402c61
Rohan <rohanchavhan17@gmail.com>
2024-02-26 11:30:48 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is Java Program");
+        System.out.println("Output from Java Program"); 
+    } 
+
+}
